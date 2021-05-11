c71c7974f36a0c190beb09bfce7b31dd3ca466d6
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-05-11 20:16:51 -0300

    Seção 5 - Criação dos respositórios e serviços

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) throw new AppError('Product not found.');
+
+    const productNameExists = await productsRepository.findByName(name);
+
+    if (productNameExists)
+      throw new AppError('There already a product with this name');
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
