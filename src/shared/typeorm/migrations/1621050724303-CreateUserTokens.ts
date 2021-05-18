568e1adc4eb56e92e715ac7882855e5d7ccaf615
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-05-17 23:05:10 -0300

    Criação dos recúrsos do modulo de clientes

--- src/shared/typeorm/migrations/1621050724303-CreateUserTokens.ts
+++ src/shared/typeorm/migrations/1621050724303-CreateUserTokens.ts
@@ -25,12 +25,12 @@ export class CreateUserTokens1621050724303 implements MigrationInterface {
           },
           {
             name: 'created_at',
-            type: 'timestamp',
+            type: 'timestamp with time zone',
             default: 'now()',
           },
           {
             name: 'updated_at',
-            type: 'timestamp',
+            type: 'timestamp with time zone',
             default: 'now()',
           },
         ],
