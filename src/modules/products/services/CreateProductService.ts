6264c337f723689495c5fcfb65ec3197d3406189
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-05-19 23:07:22 -0300

    Implementando cache com redis

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 interface IRequest {
   name: string;
@@ -17,11 +18,15 @@ class CreateProductService {
     if (productExists)
       throw new AppError('There is already one product with this name');
 
+    const redisCache = new RedisCache();
+
     const product = await productRepository.create({
       name,
       price,
       quantity,
     });
+
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     await productRepository.save(product);
 
--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -27,6 +28,10 @@ export default class UpdateProductService {
 
     if (productNameExists)
       throw new AppError('There already a product with this name');
+
+    const redisCache = new RedisCache();
+
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     product.name = name;
     product.price = price;
